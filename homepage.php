488261105e0543d285e15de420bf96c5f5d7f7a3
JSoekhoe <144214366+JSoekhoe@users.noreply.github.com>
2023-09-23 10:34:19 +0200

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>JJJ</title>
+</head>
+    <header>
+        <link href="homepage.css" rel="stylesheet" integrity="" crossorigin="anonymous">
+        <class id= 'Welkom'> 
+            <p>Welkom Bij onze portfolio</p>
+        </class>
+        <class>
+            <nav>
+                <a href="homepage.php">Home</a>
+                <a href="opdrachtenpage.php">Opdrachten</a>
+                <a href="aboutuspage.html">About Us</a>
+            </nav>
+        </class>
+    </header> 
+    <main>
+        <section>
+            <p>Inleiding</p>
+            <article>In deze Portfolio staan onze opdrachten van AD Software Development.
+                    Wij hebben op basis van al onze opdrachten een website gemaakt, hierdoor
+                    kunnen we op een efficiënte manier laten zien wat wij hebben geprogrammeerd.
+            </article>
+        </section>
+    </main>
+    <footer>
+            <a href="homepage.html">Home</a>
+            <a href="opdrachtenpage.html">Opdrachten</a>
+            <a href="aboutuspage.html">About Us</a>
+    </footer>
+ </html>
---
+++ opdrachtenpage.php
@@ -0,0 +1,1 @@
+<link href="homepage.css" rel="stylesheet" integrity="" crossorigin="anonymous">

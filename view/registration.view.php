279a897d55a220c84c7e47f86522434f4c73eefc
Joerie <soekhoe.j@gmail.com>
2023-10-27 14:37:35 +0200

    hobby_page werkend met database

--- view/registration.view.php
+++ view/registration.view.php
@@ -11,8 +11,8 @@
     <nav>
         <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/' ? 'active' : ''); ?>"
            aria-current="page" href="/">Home</a>
-        <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/login' ? 'active' : ''); ?>"
-           aria-current="page" href="/login">Login</a>
+        <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/profile' ? 'active' : ''); ?>"
+           aria-current="page" href="/profile">Profiel</a>
         <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/about' ? 'active' : ''); ?>"
            aria-current="page" href="/about">About</a>
     </nav>
@@ -34,8 +34,8 @@
 <footer>
     <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/' ? 'active' : ''); ?>"
        aria-current="page" href="/">Home</a>
-    <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/login' ? 'active' : ''); ?>"
-       aria-current="page" href="/login">Login</a>
+    <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/profile' ? 'active' : ''); ?>"
+       aria-current="page" href="/profile">Profiel</a>
     <a class="nav-link <?= ($_SERVER['REQUEST_URI'] == '/about' ? 'active' : ''); ?>"
        aria-current="page" href="/about">About</a>
 </footer>
